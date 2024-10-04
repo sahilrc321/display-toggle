366a520df0e9b6ce6fd49b8f7fcb605cc9e38b66
sahilrc321 <133751177+sahilrc321@users.noreply.github.com>
2024-10-04 11:42:02 +0530

    Delete project 1/tempCodeRunnerFile.js

--- project 1/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector(".btn")
-const container = document.querySelector(".container")
-btn.addEventListener("click" , (e) => {
-    e.preventDefault()
-    if (container.innertext == "light") {
-        container.Style.backgroundcolor = "red"
-    }
-} )
